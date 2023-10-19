b4a5881c52cabf6c73ce057748d1bf1769e89573
darynabekchanova <darynabekchanova@gmail.com>
2023-10-19 19:36:55 +0300

    fixed Wait button (ViewChild and fromEvent), minimized the number of variables

--- stopwatch/src/app/stopwatch/stopwatch.component.ts
+++ stopwatch/src/app/stopwatch/stopwatch.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { interval, Subject } from 'rxjs';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {fromEvent, interval, Subject} from 'rxjs';
 import { takeUntil, buffer, debounceTime, filter } from 'rxjs/operators';
 
 @Component({
@@ -7,35 +7,25 @@ import { takeUntil, buffer, debounceTime, filter } from 'rxjs/operators';
   templateUrl: './stopwatch.component.html',
   styleUrls: ['./stopwatch.component.css'],
 })
-export class StopwatchComponent {
+export class StopwatchComponent implements AfterViewInit {
+  @ViewChild('waitButton', {read: ElementRef}) waitButton!: ElementRef;
+
   private onDestroy$ = new Subject<void>();
-  private clicks$ = new Subject<void>();
-  private doubleClick$ = this.clicks$.pipe(
-    buffer(this.clicks$.pipe(debounceTime(300))),
-    filter((clicks) => clicks.length === 2)
-  );
+  private doubleClick$ = new Subject<void>();
 
-  formatTime() {
-    const hours = Math.floor(this.time / 3600);
-    const minutes = Math.floor((this.time % 3600) / 60);
-    const seconds = this.time % 60;
-
-    const formattedHours = this.padTime(hours);
-    const formattedMinutes = this.padTime(minutes);
-    const formattedSeconds = this.padTime(seconds);
-
-    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
-  }
-
-  private padTime(value: number): string {
-    return value.toString().padStart(2, '0');
-  }
-
-
-  timer = interval(1000);
+  timer$ = interval(1000);
   time = 0;
   running = false;
-  waiting = false;
+
+  ngAfterViewInit(): void {
+    const clicks$ = fromEvent<ElementRef>(this.waitButton.nativeElement, 'click')
+    clicks$.pipe(
+      buffer(clicks$.pipe(debounceTime(300))),
+      filter((clicks) => clicks.length === 2),
+    ).subscribe(() => {
+      this.doubleClick$.next();
+    });
+  }
 
   startStop() {
     if (this.running) {
@@ -46,8 +36,15 @@ export class StopwatchComponent {
   }
 
   startTimer() {
-    this.timer.pipe(takeUntil(this.onDestroy$)).subscribe(() => {
-      this.time++;
+    this.timer$.pipe(takeUntil(this.onDestroy$)).subscribe(() => {
+      if (this.running) {
+        this.time++;
+      }
+    });
+    this.doubleClick$.pipe(takeUntil(this.onDestroy$)).subscribe(() => {
+      if (this.running) {
+        this.stopTimer();
+      }
     });
     this.running = true;
   }
@@ -57,21 +54,9 @@ export class StopwatchComponent {
     this.running = false;
   }
 
-  wait() {
-    this.clicks$.next();
-    this.doubleClick$.pipe(takeUntil(this.onDestroy$)).subscribe(() => {
-      this.waiting = !this.waiting;
-      if (this.waiting) {
-        this.stopTimer();
-      } else {
-        this.startTimer();
-      }
-    });
-  }
 
   reset() {
-    this.time = 0;
-    this.waiting = false;
     this.stopTimer();
+    this.time = 0;
   }
 }
